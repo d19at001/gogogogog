96f11e7cae822a1ff81cc632175974a1c264198d
B19DC <theanhcia@gmail.com>
2021-11-30 21:30:37 +0700

    first commit

---
+++ ProjectLast-d6a12588c0017f0c918586366eb0249591d71bcb/app/src/main/java/com/example/projectlast/Capture.java
@@ -0,0 +1,8 @@
+package com.example.projectlast;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class Capture extends CaptureActivity {
+
+
+}
---
+++ ProjectLast-d6a12588c0017f0c918586366eb0249591d71bcb/app/src/main/java/com/example/projectlast/Info_Fragment.java
@@ -0,0 +1,134 @@
+package com.example.projectlast;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Info_Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Info_Fragment extends Fragment {
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    private String userID;
+    private Button logout;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private Object Button;
+
+    public Info_Fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Info_Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Info_Fragment newInstance(String param1, String param2) {
+        Info_Fragment fragment = new Info_Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_info_, container, false);
+        logout = (Button) v.findViewById(R.id.signOut);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity(),MainActivity.class));
+
+            }
+        });
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+        final TextView greetingTextView = (TextView) v.findViewById(R.id.greeting);
+        final TextView fullNameTextView = (TextView) v.findViewById(R.id.name);
+        final TextView emailTextView = (TextView) v.findViewById(R.id.email);
+        final TextView ageTextView = (TextView) v.findViewById(R.id.age);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+                if(userProfile != null){
+                    String fullName = userProfile.fullname;
+                    String email = userProfile.email;
+                    String age = userProfile.age;
+
+                    greetingTextView.setText("Welcome, "+ fullName+ "!");
+                    fullNameTextView.setText(fullName);
+                    emailTextView.setText(email);
+                    ageTextView.setText(age);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), "Something wrong happend!", Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+
+
+        return v;
+    }
+}
